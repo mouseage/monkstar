027a9c764a0f915af78d9c8a5843fe67f0ebc72f
Hanh Thai <hanhthai.ict@gmail.com>
2019-11-27 01:02:04 +0700

    Update reviews.js

--- reviews.js
+++ reviews.js
@@ -1,43 +1,42 @@
 $(document).ready(async function() {            
-            let reviews = await $.ajax('https://cdn.jsdelivr.net/gh/mouseage/monkstars/reviews.json');
-            let cmBlock = $('#shortcode-comments');
+let reviews = await $.ajax('https://cdn.jsdelivr.net/gh/mouseage/monkstars/reviews.json');
             
-            $.each(reviews, function(key, review) {
-                let html = `
-                    <div id="shortcode-comment`+ key +`" class="shortcode-wcpr-grid-item">
-                        <div class="shortcode-wcpr-content">`;
-                    if (review.image) {
-                        html += `
-                            <div class="shortcode-reviews-images-container">
-                                <div class="shortcode-reviews-images-wrap-left"></div>
-                                <div class="shortcode-reviews-images-wrap-right">
-                                    <img 
-                                        class="shortcode-reviews-images" 
-                                        data-original_src="`+ review.image +`" 
-                                        src="`+ review.image +`" 
-                                        alt="`+ review.customer.name +` photo review" 
-                                        title="Monkstars Review - Our happy customers 76" 
-                                    />
-                                </div>
+$.each(reviews, function(key, review) {
+    let html = `
+        <div id="shortcode-comment-`+ key +`" class="shortcode-wcpr-grid-item">
+            <div class="shortcode-wcpr-content">`;
+        if (review.image) {
+            html += `
+                <div class="shortcode-reviews-images-container">
+                    <div class="shortcode-reviews-images-wrap-left"></div>
+                    <div class="shortcode-reviews-images-wrap-right">
+                        <img 
+                            class="shortcode-reviews-images" 
+                            data-original_src="`+ review.image +`" 
+                            src="`+ review.image +`" 
+                            alt="`+ review.customer.name +` photo review" 
+                            title="Monkstars Review - Our happy customers 76" 
+                        />
+                    </div>
+                </div>
+            `;
+        }
+
+        html += `<div class="shortcode-review-content-container">
+                    <div class="shortcode-review-content-container-top">
+                        <div class="shortcode-wcpr-comment-author">`+ review.customer.name +`</div>
+                        <div class="wcpr-review-rating">
+                            <div class="star-rating">
+                                <span style="width:`+ (review.rating * 20 ) +`%">Rated <strong class="rating">`+ review.rating +`</strong> out of 5</span>
                             </div>
-                        `;
-                    }
-                            
-                    html += `<div class="shortcode-review-content-container">
-                                <div class="shortcode-review-content-container-top">
-                                    <div class="shortcode-wcpr-comment-author">`+ review.customer.name +`</div>
-                                    <div class="wcpr-review-rating">
-                                        <div class="star-rating">
-                                            <span style="width:`+ (review.rating * 20 ) +`%">Rated <strong class="rating">`+ review.rating +`</strong> out of 5</span>
-                                        </div>
-                                        <div class="wcpr-review-date">`+ review.date +`</div>
-                                    </div>
-                                </div>
-                                <div class="shortcode-wcpr-review-content">`+ review.message +`</div>
-                            </div>
+                            <div class="wcpr-review-date">`+ review.date +`</div>
                         </div>
-                    </div>`;
-                
-                cmBlock.append(html);
-            });
-        });
+                    </div>
+                    <div class="shortcode-wcpr-review-content">`+ review.message +`</div>
+                </div>
+            </div>
+        </div>`;
+
+    $('.shortcode-wcpr-grid').append(html);
+});
+});
